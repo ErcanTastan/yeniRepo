6db51ce39c703584bfbb53a7c12216f7e3005607
ErcanTastan <132624820+ErcanTastan@users.noreply.github.com>
2023-05-06 20:41:24 +0100

    Update Class01.java

--- src/main/java/Class01.java
+++ src/main/java/Class01.java
@@ -2,9 +2,7 @@ public class Class01 {
     public static void main(String[] args) {
 
         //V1 calismalarim...
-        //V2 kodlari...
-        //v3 calismalr..
-        //v4
+       
 
     }
 }
